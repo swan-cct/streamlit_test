c8368f5e887500df775bd257ab778b953179a128
Swan☝️🤓 <swansaunglwin276work@gmail.com>
2025-11-19 15:05:08 +0000

    data load function added

--- uber.py
+++ uber.py
@@ -1,4 +1,22 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("Uber Pickups in NYC")
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATE_COLUMN = 'date/time'
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    
+    return data
+
+data_load_state = st.text("Loading data...")
+data = load_data(10000)
+data_load_state = st.text("Data loaded!")
